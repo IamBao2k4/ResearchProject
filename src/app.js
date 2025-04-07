74c422d8a38738a887175603c19e8736dfaee12a
pmgbao2004@gmail.com <pmgbao2004@gmail.com>
2025-04-07 22:55:38 +0700

    update dependencies and enhance CSS styling

--- src/app.js
+++ src/app.js
@@ -3,28 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-<<<<<<< HEAD
-const db = require("./config/db");
-=======
 var session = require('express-session');
 var passport = require('./middlewares/localStrategy'); // Import the local strategy
 const db = require("./config/db");
 const hbs = require("hbs");
 const { engine } = require("express-handlebars");
->>>>>>> origin/fix_practice_logic
 
 const route = require("./routes");
 
 var app = express();
 
-<<<<<<< HEAD
-// Connect to MongoDB
-db.connect();
-
-// view engine setup
-app.set('views', path.join(__dirname, "resources", "views"));
-app.set('view engine', 'jade');
-=======
 // Register Handlebars helpers
 require('./helpers/handlebars');
 
@@ -39,35 +27,12 @@ app.set('views', path.join(__dirname, "resources", "views"));
 app.set('view engine', 'hbs');
 
 hbs.registerPartials(path.join(__dirname, "resources", "views", "partials"));
->>>>>>> origin/fix_practice_logic
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-<<<<<<< HEAD
-// Routes init
-route(app);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-=======
 
 // Session middleware
 app.use(session({
@@ -85,4 +50,3 @@ app.use(passport.session());
 route(app);
 
 module.exports = app;
->>>>>>> origin/fix_practice_logic
