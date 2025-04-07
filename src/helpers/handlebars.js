e441acc5c938fceb1fcb4f23dc59bb6f8bd6a09d
pmgbao2004@gmail.com <pmgbao2004@gmail.com>
2025-04-07 22:32:01 +0700

    fix survey if more than 3 days

--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -6,3 +6,13 @@ Handlebars.registerHelper('isToday', function (surveyDate, options) {
     const surveyDateString = new Date(surveyDate).toISOString().split('T')[0];
     return today === surveyDateString ? options.fn(this) : options.inverse(this);
 });
+
+// kiểm tra xem ngày khảo sát có nhiều hơn 3 ngày trước không
+Handlebars.registerHelper('isLessThan3Days', function (surveyDate, options) {
+    if(!surveyDate) return options.inverse(this);
+    const today = new Date();
+    const surveyDateObj = new Date(surveyDate);
+    const diffTime = Math.abs(today - surveyDateObj);
+    const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+    return diffDays <= 3 ? options.fn(this) : options.inverse(this);
+});
